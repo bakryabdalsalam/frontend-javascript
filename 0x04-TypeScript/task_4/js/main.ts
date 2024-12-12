849b33842a506abe7fd06198b4aab0a2fd5001a2
Bakry Abdalsalam <67397650+bakryabdalsalam@users.noreply.github.com>
2024-12-12 10:46:00 +0000

    add tasks from 0 to 5

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,24 @@
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentInterface;
+}
+
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student = new StudentClass("John", "Doe");
+console.log(student.workOnHomework());
+console.log(student.displayName());
